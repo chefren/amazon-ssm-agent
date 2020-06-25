05eb7b608d8f00ff0b8159f1f15aa43af05def22
chefren <chefren@users.noreply.github.com>
2020-06-26 08:39:01 +1000

    docker for test

---
+++ Docker_build.sh
@@ -0,0 +1,8 @@
+docker build -t ac3chefren/amazon-ssm-agent .
+
+# run
+# docker run -ti --rm --name test-ssm ac3chefren/amazon-ssm-agent:latest
+# docker run -ti --rm --name test-ssm ac3chefren/amazon-ssm-agent:1.0
+
+# tag
+# docker tag ac3chefren/amazon-ssm-agent:latest ac3chefren/amazon-ssm-agent:1.0
